55797e282e343d6136dcf57b16f092f82a53bf85
Ashera Cordova <ashera.cordova@gmail.com>
2025-11-21 15:11:33 +0530

    Added licensing to all files + ratingbar support.

--- ios/src/main/java/com/ashera/nestedscrollview/NestedScrollViewImpl.java
+++ ios/src/main/java/com/ashera/nestedscrollview/NestedScrollViewImpl.java
@@ -1,3 +1,18 @@
+//start - license
+/*
+ * Copyright (c) 2025 Ashera Cordova
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ */
+//end - license
 package com.ashera.nestedscrollview;
 // start - imports
 import java.util.*;
@@ -789,12 +804,12 @@ public java.util.Map<String, Object> getOnScrollChangeEventObj(NestedScrollView 
 		setScrollEnabled(uiView, objValue);
 	}
 	
-	private native void setScrollEnabled(Object nativeWidget, Object value) /*-[
+	private native void setScrollEnabled(Object nativeWidget, Object value)/*-[
 		((UIScrollView*) nativeWidget).scrollEnabled = [(JavaLangBoolean*) value booleanValue];
 	]-*/;
 	
 
-	private native boolean hasReachedEnd(int selection) /*-[
+	private native boolean hasReachedEnd(int selection)/*-[
 		UIScrollView* scrollview = ((UIScrollView*)self->uiView_);
 		return selection >= (scrollview.contentSize.height - scrollview.frame.size.height);
 	]-*/;
